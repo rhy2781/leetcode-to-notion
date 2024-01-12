b9cbf1712e5f582d235468fb93fc0fcccf8c4f94
Robert Yamasaki <rhy2781@rit.edu>
2024-01-12 14:02:44 -0800

    updated format for tabs

--- notion.js
+++ notion.js
@@ -1,43 +1,43 @@
+
 var url = 'https://leetcode.com/graphql';
 var operationName = 'singleQuestionTopicTags';
 var s = 'check-if-n-and-its-double-exist';
 
 var query = `
-    query singleQuestionTopicTags($titleSlug:String!){
-        question(titleSlug:$titleSlug){
-            topicTags{
-                name
-                slug
-            }
-        }
-    }
-`
+      query singleQuestionTopicTags($titleSlug:String!){
+          question(titleSlug:$titleSlug){
+              topicTags{
+                  name
+                  slug
+              }
+          }
+      }
+  `
 
 
 var options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    operationName: operationName ,
-    query:query,
-    variables: {
-        titleSlug: "add-two-numbers"
-    }
-  })
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		operationName: operationName,
+		query: query,
+		variables: {
+			titleSlug: "add-two-numbers"
+		}
+	})
 }
 
 
-console.log(options)
-
 fetch(url, options)
-  .then(response => {
-    if (!response.ok) throw new Error('HTTP error! Status: ' + response.status);
-    return response.json();
-  })
-  .then((data) =>{
-    response = data["data"]
-    console.log(response["question"]["topicTags"])
-    })
-  .catch(error => console.error('Error:', error));
+	.then(response => {
+		if (!response.ok) throw new Error('HTTP error! Status: ' + response.status);
+		return response.json();
+	})
+	.then((data) => {
+		response = data["data"]
+		console.log(response["question"]["topicTags"])
+	})
+	.catch(error => console.error('Error:', error));
+
