c96cfe6a910a49d58dfcaad8b0aff4bcef8a5e84
Robert Yamasaki <rhy2781@rit.edu>
2024-01-12 19:26:08 -0800

    formatted introductory code for notion page creation, values such as name and tags are not inputted correctly

--- notion.js
+++ notion.js
@@ -1,10 +1,16 @@
+require('dotenv').config() 
+
 const graphql = 'https://leetcode.com/graphql';
 const url = process.argv[2]
 const problemSlug = process.argv[2].split("/")[4]
+const notion_url = 'https://api.notion.com/v1/pages'
+
 
 /* 
 	Get the tagged topics for a question on leetcode via graphql
 	required usage on command line $node notion.js https://leetcode.com/~~~
+
+	[topic, topic1, topic2, topic3, ... ]
 */
 function getTags(){
 	var operationName = 'singleQuestionTopicTags';
@@ -48,6 +54,10 @@ function getTags(){
 
 /*
 	Get the question title and number to use for database and page creation entry
+	{
+		title: "....",
+		number: ...
+	}
 */
 function getQuestionTitle(){
 	var operationName = 'questionTitle';
@@ -96,7 +106,62 @@ function getQuestionTitle(){
 		.catch(error => console.error('Error:', error));
 }
 
+/*
+	Submit request via notion api to create a page within my personal database
+*/
+function createNotionPage(tags, questionTitle){
+	var ms = []
+	tags.forEach(element => {
+		ms.push({name: element})
+	});
 
 
-getTags().then(tags => console.log(tags))
-getQuestionTitle().then(res => console.log(res))
+	options = {
+    	method: 'POST',
+    	headers: {
+        	'Content-Type': 'application/json',
+        	'Authorization': `Bearer ${process.env.notion_secret}`,
+        	'Notion-Version': '2022-06-28'
+    	},
+    	body: JSON.stringify({
+        	parent:{
+            	type: "database_id",
+            	database_id: process.env.database_id,
+        	},
+        	properties:{
+            	"Name":{
+                	type: "title",
+                		title: [{
+                        	type: "text",
+                        	text: {
+                            	content: `${questionTitle["number"]}. ${questionTitle["title"]}`
+							}
+                    	}]
+            		},
+				"Tags":{
+					type : "multi_select",
+					multi_select: ms
+					// multi_select : {
+					// 	options: ms
+					// }
+				}
+			}
+    	})
+	}
+
+	fetch(notion_url, options)
+	    .then(res => res.json())
+	    .then(data => console.log(data))
+	    .catch(err => console.log(err))
+
+}
+
+
+
+async function main(){
+	const tags = await getTags()
+	const questionTitle = await getQuestionTitle()\
+	const res = await createNotionPage(tags, questionTitle)
+}
+
+main()
